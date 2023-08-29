e49f8bc086d3c3a00d93971563d49bc6e38f147b
DenisNgaitia <dennisngatiantlax@gmail.com>
2023-08-30 01:43:20 +0300

    new

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -23,10 +23,6 @@ void print_python_bytes(PyObject *p)
 	string = ((PyBytesObject *)p)->ob_sval;
 
 	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", string);
-
-	if (size >= 10)
-		limit = 10;
 	else
 		limit = size + 1;
 
@@ -34,13 +30,6 @@ void print_python_bytes(PyObject *p)
 
 	for (i = 0; i < limit; i++)
 		if (string[i] >= 0)
-			printf(" %02x", string[i]);
-		else
-			printf(" %02x", 256 + string[i]);
-
-	printf("\n");
-}
-
 /**
  * print_python_list - This one Prints list information
  * @p: this is Python Object
@@ -58,13 +47,3 @@ void print_python_list(PyObject *p)
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
-
-	for (i = 0; i < size; i++)
-	{
-		obj = ((PyListObject *)p)->ob_item[i];
-		printf("Element %ld: %s\n", i, ((obj)->ob_type)->tp_name);
-		if (PyBytes_Check(obj))
-			print_python_bytes(obj);
-	}
-
