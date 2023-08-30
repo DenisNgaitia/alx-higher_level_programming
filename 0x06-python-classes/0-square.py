cf770bb658d0a7ee4915734c52ef094d5e053462
DenisNgaitia <dennisngatiantlax@gmail.com>
2023-08-30 03:24:28 +0300

    new

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module that defines a Square class"""
+
+
+class Square:
+    """Square:
+
+    This class defines a square
+    """
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+Basic User Class - README
+Welcome to the Basic User Class project! This project serves as an introduction to creating a simple user class in Python.
